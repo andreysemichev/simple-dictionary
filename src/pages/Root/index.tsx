ba7772c2090777acafa428895bcb1b7f5ab53ef5
andreysemichev <andreysemichev@icloud.com>
2021-06-16 08:32:04 +0400

    Add search component

--- src/pages/Root/index.tsx
+++ src/pages/Root/index.tsx
@@ -1,6 +1,23 @@
+import cn from "classnames";
+
+import Search from "components/Search";
+import { ChangeEvent, useState } from "react";
+
+import styles from "./styles.module.scss"
+
 const Index: React.FC = () => {
+    const [searchValue, setSearchValue] = useState<string>("");
+
+    const handleUpdateSearchValue = (event: ChangeEvent<HTMLInputElement>) => {
+        setSearchValue(event.target.value);
+    }
+
     return (<>
-        <h1>Root</h1>
+        <main className={styles.main}>
+            <div className={cn(styles.mainInner, styles.container)}>
+                <Search searchValue={searchValue} handleUpdateSearchValue={handleUpdateSearchValue} />
+            </div>
+        </main>
     </>);
 }
 
